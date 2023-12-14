0ab88f423cab2593193e2a3f82c53aac0dc92d18
unknown <ankurs753@gmail.com>
2023-12-14 18:37:47 +0530

    Commit

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,12 +1,12 @@
 var sliderMain = document.querySelector('.slider-main');
 let item = sliderMain.getElementsByClassName('item');
 
-function next(){
+function next() {
     sliderMain.append(item[0]);
 }
 
-function prev(){
-    sliderMain.prepend(item[item.length-1]);
+function prev() {
+    sliderMain.prepend(item[item.length - 1]);
 }
 
 // setInterval(next,1000)
